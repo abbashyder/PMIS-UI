51a0559414e45b587ff68e762d277299ec9bd8ab
Abbas Hyder <abbashyder1109@gmail.com>
2023-11-05 01:15:36 +0530

    added metadata

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,19 @@
+import { Metadata } from 'next';
 import Layout from '@/layout/layout';
 
 interface AppLayoutProps {
     children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+    title: 'PMIS',
+    description: 'Aarvee PMIS',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    icons: {
+        icon: '/favicon.ico'
+    }
+};
 
 export default function AppLayout({ children }: AppLayoutProps) {
     return <Layout>{children}</Layout>;
