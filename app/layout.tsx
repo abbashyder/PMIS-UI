b4184133d8a16720920b902a79ee810a0f4377e4
Abbas Hyder <abbashyder1109@gmail.com>
2023-11-05 01:05:02 +0530

    more cleanup - blank slate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { LayoutProvider } from '../layout/context/layoutcontext';
+import { LayoutProvider } from '@/layout/context/layoutcontext';
 import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
-import '../styles/layout/layout.scss';
+import '@/styles/layout/layout.scss';
+import 'primereact/resources/primereact.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 interface RootLayoutProps {
